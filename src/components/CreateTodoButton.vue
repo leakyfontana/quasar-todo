d4780890d47a73c571ad1f54ad7feff323a9296f
Xander Dyer <xander.dyer@titantechnologies>
2023-06-05 12:00:50 -0400

    add quasar plugins

--- src/components/CreateTodoButton.vue
+++ src/components/CreateTodoButton.vue
@@ -1,3 +1,27 @@
 <template>
-  <q-btn />
+  <q-btn @click="handleClick" />
 </template>
+
+<script setup>
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+
+const handleClick = () => {
+  $q.dialog({
+    title: 'Create Todo',
+    prompt: {
+      model: '',
+      type: 'text'
+    }
+  }).onOk(createTodo)
+}
+
+const createTodo = (data) => {
+  $q.notify({
+    message: `Todo created: ${data}`,
+    icon: 'mdi-check',
+    color: 'positive'
+  })
+}
+</script>
