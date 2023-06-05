3b02c49d49d81e8dabe92bc263f9aae4024c41ef
Xander Dyer <xander.dyer@titantechnologies>
2023-06-05 10:03:35 -0400

    ui skeleton pt 1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,48 @@
 <template>
-  <q-page padding>
-    <q-select
-      v-model="selected"
-      filled
-      :options="['X', 'C', 'B', 'S']"
-    />
+  <q-page
+    class="row"
+    padding
+  >
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          dense
+          :model-value="search"
+          class="full-width"
+          borderless
+          placeholder="search"
+        >
+          <template #append>
+            <q-icon
+              name="mdi-magnify"
+            />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true" />
+          </q-item-section>
+          <q-item-section>
+            Go Shopping
+          </q-item-section>
+          <q-item-section side>
+            <q-btn
+              flat
+              round
+              size="sm"
+              icon="mdi-delete"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-const selected = ref(null)
+const search = ref('')
 </script>
